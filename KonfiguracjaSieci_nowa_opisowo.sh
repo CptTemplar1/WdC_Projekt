926d3643fbdba31f3bebebe9dee07c215f43d986
CptTemplar1 <michal2001k@op.pl>
2024-12-19 18:23:29 +0100

    Dodano konfigurację ZPF, VLAN, i STP

--- KonfiguracjaSieci_nowa_opisowo.sh
+++ KonfiguracjaSieci_nowa_opisowo.sh
@@ -131,7 +131,7 @@ line vty 5 15
 end
 
 
-#Router1
+#R1
 enable
 configure terminal
 
@@ -156,6 +156,7 @@ interface GigabitEthernet0/0
  no ip address
  duplex auto
  speed auto
+ no shutdown
  exit
 
 interface GigabitEthernet0/0.10
@@ -174,7 +175,7 @@ interface GigabitEthernet0/1
  no ip address
  duplex auto
  speed auto
- shutdown
+ no shutdown
  exit
 
 interface Serial0/0/0
@@ -551,6 +552,494 @@ line vty 5 15
  password cisco
  login local
  transport input ssh
+ exit
+
+end
+
+
+
+############################################################################################################
+#VLANs
+#S1
+enable
+configure terminal
+
+service timestamps log datetime msec
+no service timestamps debug datetime msec
+service password-encryption
+
+hostname S1
+
+enable password class
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface FastEthernet0/1
+ switchport mode trunk
+ no shutdown
+ exit
+
+interface FastEthernet0/2
+ switchport access vlan 10
+ switchport mode access
+ no shutdown
+ exit
+
+interface FastEthernet0/3
+ switchport access vlan 20
+ switchport mode access
+ no shutdown
+ exit
+
+interface Vlan1
+ ip address 192.168.1.2 255.255.255.0
+ no shutdown
+ exit
+
+interface Vlan2
+ ip address 192.168.1.5 255.255.255.252
+ no shutdown
+ exit
+
+interface Vlan3
+ ip address 192.168.1.9 255.255.255.252
+ no shutdown
+ exit
+
+ip default-gateway 192.168.1.1
+
+line con 0
+ password cisco
+ login
+ exit
+
+line vty 0 4
+ password cisco
+ login
+ exit
+
+line vty 5 15
+ password cisco
+ login
+ exit
+
+end
+
+
+############################################################################################################
+#ZPF
+#R4
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname R4
+
+enable password class
+
+no ip cef
+no ipv6 cef
+
+username cisco secret cisco
+
+license udi pid CISCO1941/K9 sn FTX1524B1Z1-
+#ZPF wymaga tej paczki
+license boot module c1900 technology-package securityk9
+
+spanning-tree mode pvst
+
+#ZPF
+zone security IN-ZONE
+ exit
+zone security OUT-ZONE
+ exit
+access-list 101 permit ip 198.168.1.0 0.0.0.255 any
+class-map type inspect match-all IN-NET-CLASS-MAP
+ match access-group 101
+ exit
+policy-map type inspect IN-2-OUT-PMAP
+ class type inspect IN-NET-CLASS-MAP
+  inspect
+ exit
+ exit
+zone-pair security IN-2-OUT-ZPAIR source IN-ZONE destination OUT-ZONE
+ service-policy type inspect IN-2-OUT-PMAP
+ exit
+
+interface GigabitEthernet0/0
+ ip address 198.168.1.1 255.255.255.0
+ #ZPF
+ zone-member security IN-ZONE
+ duplex auto
+ speed auto
+ no shutdown
+ exit
+
+interface GigabitEthernet0/1
+ ip address 192.168.3.1 255.255.255.0
+ duplex auto
+ speed auto
+ no shutdown
+ exit
+
+interface Serial0/0/0
+ ip address 7.0.0.2 255.0.0.0
+ #ZPF
+ zone-member security OUT-ZONE
+ clock rate 2000000
+ no shutdown
+ exit
+
+interface Serial0/0/1
+ ip address 10.2.2.1 255.255.255.252
+ clock rate 2000000
+ no shutdown
+ exit
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+router rip
+ network 7.0.0.0
+ network 197.168.1.0
+ network 198.168.1.0
+ exit
+
+ip classless
+ip route 0.0.0.0 0.0.0.0 10.2.2.2 
+
+ip flow-export version 9
+
+line con 0
+ login
+ exit
+
+line aux 0
+ exit
+
+line vty 0 4
+ password ciscovtypa55
+ login
+ exit
+
+end
+
+
+#S10
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S10
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface FastEthernet0/1
+ no shutdown
+ exit
+
+interface GigabitEthernet0/1
+ no shutdown
+ exit
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
+ exit
+
+end
+
+
+############################################################################################################
+#SPAN
+#S9
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+service password-encryption
+
+hostname S9
+
+enable password class
+
+#SPAN
+monitor session 1 source interface Fa0/1
+monitor session 1 destination interface Fa0/2
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface Vlan1
+ ip address 194.168.1.2 255.255.255.0
+ no shutdown
+ exit
+
+ip default-gateway 194.168.1.1
+
+line con 0
+ password cisco
+ login
+ exit
+
+line vty 0 4
+ password cisco
+ login
+ exit
+
+line vty 5 15
+ password cisco
+ login
+ exit
+
+end
+
+
+
+############################################################################################################
+#STP
+#S4
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S4
+
+no ip domain-lookup
+
+#STP
+spanning-tree mode pvst
+spanning-tree extend system-id
+spanning-tree vlan 1 root primary
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ login
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
+ exit
+
+end
+
+
+#S5
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S5
+
+no ip domain-lookup
+
+#STP
+spanning-tree mode pvst
+spanning-tree extend system-id
+spanning-tree vlan 1 root secondary
+
+interface FastEthernet0/2
+ spanning-tree guard root
+ exit
+
+interface FastEthernet0/3
+ spanning-tree guard root
+ exit
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ login
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
+ exit
+
+end
+
+
+
+#S6
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S6
+
+no ip domain-lookup
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface FastEthernet0/3
+ spanning-tree guard root
+ exit
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ login
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
+ exit
+
+end
+
+
+
+#S7
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S7
+
+no ip domain-lookup
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface FastEthernet0/1
+ exit
+interface FastEthernet0/2
+ exit
+
+#STP
+interface FastEthernet0/3
+ spanning-tree portfast
+ spanning-tree bpduguard enable
+ exit
+interface FastEthernet0/4
+ spanning-tree portfast
+ spanning-tree bpduguard enable
+ exit
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ login
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
+ exit
+
+end
+
+
+
+#S8
+enable
+configure terminal
+
+no service timestamps log datetime msec
+no service timestamps debug datetime msec
+no service password-encryption
+
+hostname S8
+
+no ip domain-lookup
+
+spanning-tree mode pvst
+spanning-tree extend system-id
+
+interface FastEthernet0/1
+ exit
+interface FastEthernet0/2
+ exit
+
+#STP
+interface FastEthernet0/3
+ spanning-tree portfast
+ spanning-tree bpduguard enable
+ exit
+interface FastEthernet0/4
+ spanning-tree portfast
+ spanning-tree bpduguard enable
+ exit
+
+
+interface Vlan1
+ no ip address
+ shutdown
+ exit
+
+line con 0
+ login
+ exit
+
+line vty 0 4
+ login
+ exit
+
+line vty 5 15
+ login
  exit
 
 end
